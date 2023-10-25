5a0c59e3e2acf15f85a93bd458c3a230eb78e772
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2023-10-24 22:15:21 -0700

    Add files via upload

---
+++ for-each.php
@@ -0,0 +1,57 @@
+<?php
+//our wine list, woould it not be an array of wines???
+
+
+
+echo '<h2>This will be my wine list</h2>';
+
+$wines = array(
+    'Cabernet',
+    'Merlot',
+    'Syrah',
+    'Malbec',
+    'Red Blend'
+);
+
+//since we cannot echo our wines array, we will be using a foreach loop!
+
+echo '<ul>';
+foreach($wines as $key) {
+    echo'<li> '.$key.' </li>';
+}
+echo '</ul>';
+
+echo '<h2>Movies and Shows list which will have both a key and a value</h2>';
+//[] means array
+$shows = [
+    'Apple TV1' => 'Severance',
+    'Apple TV2' => 'For all Mankind',
+    'Showtime1' => 'City on a Hill',
+    'Showtime2' => 'Homeland',
+    'Movie' => 'Top Gun Maverick',
+    'HBO MAX' => 'Hacks'
+
+];
+
+echo '<ul>';
+foreach($shows as $key => $value){
+    echo'<li> <b>'.$key.'</b>: '.$value.' </li>';
+}
+echo '</ul>';
+
+echo '<h2>Time for our navigation that will again have both a key and a value</h2>';
+
+$nav = array(
+    'index.php' => 'Home',
+    'about.php' => 'About',
+    'daily.php' => 'Daily',
+    'project.php' => 'Project',
+    'contact.php' => 'Contact',
+    'gallery.php' => 'Gallery',
+);
+
+echo '<ul>';
+foreach($nav as $key => $value){
+    echo'<li><a href=" '.$key.' "> '.$value.'</a></li>';
+}
+echo '</ul>';
