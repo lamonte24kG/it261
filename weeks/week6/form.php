40988843649889eb35af4259babb99c660bc9380
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2023-11-07 19:39:04 -0800

    Add files via upload

--- weeks/week6/form.php
+++ weeks/week6/form.php
@@ -17,138 +17,65 @@
             Contact LaMonte
             </legend>
             <label>First Name</label>
-            <input type="text" name="first_name" value="
-                <?php if(isset($_POST['first_name']))
-                    echo htmlspecialchars($_POST['first_name'])
-                ;?>
-            ">        
+            <input type="text" name="first_name" value="<?php if(isset($_POST['first_name'])) echo htmlspecialchars($_POST['first_name']);?>">        
 
             <label>Last Name</label>
-            <input type="text" name="last_name" value="
-                <?php if(isset($_POST['last_name']))
-                    echo htmlspecialchars($_POST['last_name'])
-                ;?>
-            ">     
+            <input type="text" name="last_name" value="<?php if(isset($_POST['last_name'])) echo htmlspecialchars($_POST['last_name']);?>">     
 
             <label>Email</label>
-            <input type="email" name="email" value="
-                <?php if(isset($_POST['email']))
-                    echo htmlspecialchars($_POST['email'])
-                ;?>
-            ">  
+            <input type="email" name="email" value="<?php if(isset($_POST['email'])) echo htmlspecialchars($_POST['email']);?>">  
 
             <label>Gender</label>
 
                 <ul>
-                    <li><input type="radio" name="gender" value="female"
-                        <?php if(isset($_POST['gender']) && $_POST['gender'] == 'female') 
-                            echo 'checked="checked"'
-                        ;?>>Female</li>
+                <li><input type="radio" name="gender" value="female"<?php if(isset($_POST['gender']) && $_POST['gender'] == 'female')  echo 'checked="checked"';?>>Female</li>
 
-                    <li><input type="radio" name="gender" value="male"
-                        <?php if(isset($_POST['gender']) && $_POST['gender'] == 'male') 
-                            echo 'checked="checked"'
-                        ;?>>Male</li>    
+                <li><input type="radio" name="gender" value="male"<?php if(isset($_POST['gender']) && $_POST['gender'] == 'male')  echo 'checked="checked"';?>>Male</li>    
 
-                    <li><input type="radio" name="gender" value="neither"
-                        <?php if(isset($_POST['gender']) && $_POST['gender'] == 'neither') 
-                            echo 'checked="checked"'
-                        ;?>>Neither</li>
+                <li><input type="radio" name="gender" value="neither"<?php if(isset($_POST['gender']) && $_POST['gender'] == 'neither') echo 'checked="checked"';?>>Neither</li>
                 </ul>
-        <label>Phone</label>
-            <input type="tel" name="phone" value="
-            <?php if(isset($_POST['phone'])) echo htmlspecialchars($_POST['phone']) 
-            ;?>">
+
+            <label>Phone</label>
+            <input type="tel" name="phone" value="<?php if(isset($_POST['phone'])) echo htmlspecialchars($_POST['phone']);?>">
 
         <label>Favorite Wines</label>
             <ul>
-                <li><input type="checkbox" name="wines[]" value="cab"
-                    <?php if(isset($_POST['wines']) && in_array('cab',$wines))
-                        echo 'checked = "checked"' 
-                         ;?>    
-                >Cabernet</li>
+                <li><input type="checkbox" name="wines[]" value="cab"<?php if(isset($_POST['wines']) && in_array('cab',$wines)) echo 'checked = "checked"' ;?>>Cabernet</li>
 
-                <li><input type="checkbox" name="wines[]" value="merlot"
-                    <?php if(isset($_POST['wines']) && in_array('merlot',$wines))
-                        echo 'checked = "checked"' 
-                         ;?>    
+                <li><input type="checkbox" name="wines[]" value="merlot"<?php if(isset($_POST['wines']) && in_array('merlot',$wines)) echo 'checked = "checked"';?>    
                 >Merlot</li>
 
-                <li><input type="checkbox" name="wines[]" value="syrah"
-                    <?php if(isset($_POST['wines']) && in_array('syrah',$wines))
-                        echo 'checked = "checked"' 
-                         ;?>    
-                >Syrah</li>
+                <li><input type="checkbox" name="wines[]" value="syrah"<?php if(isset($_POST['wines']) && in_array('syrah',$wines)) echo 'checked = "checked"';?>>Syrah</li>
 
-                <li><input type="checkbox" name="wines[]" value="malbec"
-                    <?php if(isset($_POST['wines']) && in_array('malbec',$wines))
-                        echo 'checked = "checked"' 
-                         ;?>    
-                >Malbec</li>
+                <li><input type="checkbox" name="wines[]" value="malbec"<?php if(isset($_POST['wines']) && in_array('malbec',$wines)) echo 'checked = "checked"';?>>Malbec</li>
 
-                <li><input type="checkbox" name="wines[]" value="red"
-                    <?php if(isset($_POST['wines']) && in_array('red',$wines))
-                        echo 'checked = "checked"' 
-                         ;?>    
-                >Red Blend</li>
+                <li><input type="checkbox" name="wines[]" value="red"<?php if(isset($_POST['wines']) && in_array('red',$wines)) echo 'checked = "checked"';?>>Red Blend</li>
             </ul>    
 
         <label>Regions</label>    
 
             <select name="regions">
-                <option value=""
-                <?php if(isset($_POST['regions']) && is_null($_POST['regions']))  
-                    echo 'selected="unselected"' 
-                ;?>
-                >Select One!</option>
+            <option value=""<?php if(isset($_POST['regions']) && is_null($_POST['regions'])) echo 'selected="unselected"' ;?>>Select One!</option>
 
-                <option value="nw"
-                <?php if(isset($_POST['regions']) && $_POST['regions'] =="nw") 
-                    echo 'selected="unselected"' 
-                ;?>
-                >Northwest</option>
+            <option value="nw"<?php if(isset($_POST['regions']) && $_POST['regions'] =="nw") echo 'selected="unselected"';?>>Northwest</option>
 
-                <option value="sw"
-                <?php if(isset($_POST['regions']) && $_POST['regions'] =="sw") 
-                    echo 'selected="unselected"' 
-                ;?>
-                >Southwest</option>
+            <option value="sw"<?php if(isset($_POST['regions']) && $_POST['regions'] =="sw") echo 'selected="unselected"';?>>Southwest</option>
 
-                <option value="mw"
-                <?php if(isset($_POST['regions']) && $_POST['regions'] =="mw") 
-                    echo 'selected="unselected"' 
-                ;?>
-                >Midwest</option>
+            <option value="mw"<?php if(isset($_POST['regions']) && $_POST['regions'] =="mw") echo 'selected="unselected"';?>>Midwest</option>
 
-                <option value="ne"
-                <?php if(isset($_POST['regions']) && $_POST['regions'] =="ne") 
-                    echo 'selected="unselected"' 
-                ;?>
-                >Northeast</option>
+            <option value="ne"
+            <?php if(isset($_POST['regions']) && $_POST['regions'] =="ne") echo 'selected="unselected"';?>>Northeast</option>
 
-                <option value="se"
-                <?php if(isset($_POST['regions']) && $_POST['regions'] =="se") 
-                    echo 'selected="unselected"' 
-                ;?>
-                >Southeast</option>
+            <option value="se"<?php if(isset($_POST['regions']) && $_POST['regions'] =="se") echo 'selected="unselected"';?>>Southeast</option>
 
             </select>
 
-
             <label>Comments</label>
-            <textarea name="comments">
-                <?php if(isset($_POST['comments']))
-                    echo htmlspecialchars($_POST['comments'])
-                ;?>
-            </textarea>
+            <textarea name="comments"><?php if(isset($_POST['comments'])) echo htmlspecialchars($_POST['comments']);?></textarea>
 
             <label>Privacy</label>
             <ul>
-                <li><input type="radio" name="privacy" value="yes" 
-                    <?php if(isset($_POST['privacy']) && $_POST['privacy'] == "yes") 
-                        echo 'checked="checked"' ;?>
-                >Yes</li>
-
+                <li><input type="radio" name="privacy" value="yes"<?php if(isset($_POST['privacy']) && $_POST['privacy'] == "yes")  echo 'checked="checked"';?>>Yes</li>
             </ul>
 
             <input type="submit" name="Send it!">
