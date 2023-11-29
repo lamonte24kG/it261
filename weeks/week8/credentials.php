771e36f134c5eddb63212b6bbb152da832030455
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2023-11-29 00:12:57 -0800

    Add files via upload

---
+++ weeks/week8/credentials.php
@@ -0,0 +1,7 @@
+<?php //my credentials that will allow me to connect to the database!!!
+
+
+define('DB_NAME', 'it261_db');
+define('DB_USER', 'user2_it261');
+define('DB_PASSWORD', 'user2login');
+define('DB_HOST', 'niederer.pdx1-mysql-a7-4b.dreamhost.com');
