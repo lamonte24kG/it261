e990f2a2f46b403ab97313a353eab2a910e5c5b5
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2023-12-05 19:47:37 -0800

    Add files via upload

---
+++ website/about.php
@@ -0,0 +1,31 @@
+<?php
+include ('config.php');
+include('./includes/header.php'); ?>
+
+<div id="wrapper">
+
+<div id="project">
+<img src="images/dbtable.jpg" alt="Table">
+</div> 
+<!--end div project-->
+
+
+<main>
+
+<h1>Welcome to our Aobut Page!</h1>
+<p>Donec et porttitor elit, a sollicitudin leo. Fusce varius sem elementum sem pharetra varius. Sed rutrum, mi vel fermentum finibus, purus mi malesuada mauris, ullamcorper pellentesque quam eros a ipsum. Duis volutpat nulla tincidunt dapibus fringilla.</p>
+
+</main>
+
+<aside>
+<h3>Aside</h3>
+
+</aside>
+
+</div>
+<!--end wrapper-->
+</body>
+</html>
+<!--footer starts here-->
+<?php
+include('./includes/footer.php');
