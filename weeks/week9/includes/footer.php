89e0ffe62b154a4de5806a33cfef16c5efbf501e
lamonte24kG <141269004+lamonte24kG@users.noreply.github.com>
2023-12-13 15:57:33 -0800

    Add files via upload

---
+++ weeks/week9/includes/footer.php
@@ -0,0 +1,20 @@
+<footer>
+    <ul>
+        <li>Copyright &copy;
+            2023</li>
+        <li>All Rights Reserved</li>
+        <li><a href="../../index.php">Web Design by LaMonte Golden</a></li>
+        <li><a id="html-checker" href="#">HTML Validation</a></li>
+        <li><a id="css-checker" href="#">CSS Validation</a></li>
+        <li><a id="GitHub" href="https://github.com/lamonte24kG/it261">GitHub</a></li>
+        </ul>
+        
+        <script>
+                document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+                document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+        </script>
+
+</footer>
+
+</body>
+</html>
